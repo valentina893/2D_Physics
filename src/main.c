4db71b8d0dcbf4909a6a2a7ac059489766ce8d03
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-02-21 20:47:34 -0600

    fixed early prototype

--- src/main.c
+++ src/main.c
@@ -7,6 +7,30 @@
 #define window_width 640
 #define window_height 480
 
+void update_rgbd(rgbd2* rgbd_array, int arr_size, int pos_held) {
+    for (int i = 0; i < arr_size; i++) {
+        if (i != pos_held) {
+            rgbd2_applyForce(&rgbd_array[i], 0, 980.0f * rgbd_array[i].mass);
+            rgbd2_integrateEuler(&rgbd_array[i], 1.0f / 60.0f);
+            rgbd2_windowCollision(&rgbd_array[i], window_width+(50/2), window_height+(50/2));
+        }
+    }
+
+    for (int i = 0; i < arr_size-1; i++) {
+        for (int j = 1; j < arr_size; j++) {
+            rgbd2_objectCollision(&rgbd_array[i], &rgbd_array[j]);
+        }
+    }
+}
+
+void draw(renderer* _renderer, rgbd2* rgbd_array, int arr_size) {
+    renderer_clear(_renderer, 0, 0, 0, 0);
+    for (int i = 0; i < arr_size; i++) {
+        renderer_drawRect(_renderer, rgbd_array[i].pos.x-(50/2), rgbd_array[i].pos.y-(50/2), rgbd_array[i].width, rgbd_array[i].height, 100, 100, 100, 0);
+    }
+    renderer_present(_renderer);
+}
+
 int main() {
 
     renderer _renderer;
@@ -19,55 +43,64 @@ int main() {
 
     rgbd2* rgbd_array = (rgbd2*)malloc(sizeof(rgbd2) * 10);
     int arr_size = 0;
+    //rgbd_array[0] = rgbd2_create(window_width/2, window_height/2, 100, 0, 50, 50);
+    //arr_size++;
 
     int running = 1;
 
+    int pos_held = -1;
+
     while (running) {
+
         input_read(&_input, &running);
 
-        int x, y;
-        int rgbd_held = -1;
+        if (_input.mouse_left_pressed) {
 
-        if (_input.mouse_left_press) {
-            SDL_GetMouseState(&x, &y);
+            int clicked_idx = -1;
+
+            // check if object was clicked
             for (int i = 0; i < arr_size; i++) {
-                if (x >= rgbd_array[i].pos.x-rgbd_array[i].width/2 && x <= rgbd_array[i].pos.x+rgbd_array[i].width/2 && y >= rgbd_array[i].pos.y-rgbd_array[i].height/2 && y <= rgbd_array[i].pos.x+rgbd_array[i].height/2) {
-                    rgbd_held = i;
+                if (_input.mouse_x >= rgbd_array[i].pos.x-rgbd_array[i].width/2
+                && _input.mouse_x <= rgbd_array[i].pos.x+rgbd_array[i].width/2
+                && _input.mouse_y >= rgbd_array[i].pos.y-rgbd_array[i].height/2
+                && _input.mouse_y <= rgbd_array[i].pos.y+rgbd_array[i].height/2) {
+                    clicked_idx = i;
+                    //pos_held = i;
                     break;
+                }   
+            }
+
+            if (clicked_idx != -1) {
+                // dragging object
+                pos_held = clicked_idx;
+            } else {
+                if (arr_size < 10) {
+                    rgbd_array[arr_size] = rgbd2_create(_input.mouse_x, _input.mouse_y, 100, 0, 50, 50);
+                    arr_size++;
                 }
             }
-            // spawn an object
-            if (rgbd_held == -1 && arr_size < 10) {
-                rgbd_array[arr_size] = rgbd2_create(x-(50/2), y-(50/2), 100, 0, 50, 50);
-                arr_size++;
-                _input.mouse_left_press = 0;
-            }
+
         }
 
-        for (int i = 0; i < arr_size; i++) {
-            if (i == rgbd_held) {
-                rgbd_array[i].pos.x = x;
-                rgbd_array[i].pos.y = y;
-            } else {
-                rgbd2_applyForce(&rgbd_array[i], 0, 980.0f * rgbd_array[i].mass);
-                rgbd2_integrateEuler(&rgbd_array[i], 1.0f / 60.0f);
-                rgbd2_windowCollision(&rgbd_array[i], window_width, window_height);
-            }
+        if (_input.mouse_left_released) {
+            pos_held = -1;
         }
 
-        for (int i = 0; i < arr_size-1; i++) {
-            for (int j = 1; j < arr_size; j++) {
-                rgbd2_objectCollision(&rgbd_array[i], &rgbd_array[j]);
-            }
+        if (pos_held != -1) {
+            //for (int i = 0; i < arr_size; i++) {
+                //if (i == pos_held) {
+                    rgbd_array[pos_held].vel = vec2_mult(rgbd_array[pos_held].vel, (vec2){0, 0});
+                    rgbd_array[pos_held].pos.x = _input.mouse_x;// - rgbd_array[pos_held].width / 2;
+                    rgbd_array[pos_held].pos.y = _input.mouse_y;// - rgbd_array[pos_held].height / 2;
+                //}
+            //}
         }
 
-        renderer_clear(&_renderer, 0, 0, 0, 0);
-        for (int i = 0; i < arr_size; i++) {
-            renderer_drawRect(&_renderer, rgbd_array[i].pos.x, rgbd_array[i].pos.y, rgbd_array[i].width, rgbd_array[i].height, 100, 100, 100, 0);
-        }
-        renderer_present(&_renderer);
+        update_rgbd(rgbd_array, arr_size, pos_held);
+        draw(&_renderer, rgbd_array, arr_size);
     }
 
+    free(rgbd_array);
     renderer_delete(&_renderer);
     return 0;
 
